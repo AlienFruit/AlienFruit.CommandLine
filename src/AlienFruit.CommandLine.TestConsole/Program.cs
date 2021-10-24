cf08806c73cf843b4cd2b2564fae07e6f726b77c
Alexandr Prokopovich <alienfruit@ya.ru>
2021-10-24 22:36:22 +0200

    chore: add test projects

---
+++ src/AlienFruit.CommandLine.TestConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using AlienFruit.CommandLine.Abstractions;
+using AlienFruit.CommandLine.Attributes;
+using System;
+
+namespace AlienFruit.CommandLine.TestConsole
+{
+    [Command("test1", Help = "this is help")]
+    public class TestCommand : ICommand
+    {
+        [Option('p', "prop", Help = "help")]
+        public int IntProp { get; set; }
+
+        [Option('s', "second", Help = "help", Required = true)]
+        public int SecondProperty { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new Processor()
+                .RegisterCommand<TestCommand>(x =>
+                {
+                    Console.WriteLine($"Name:{x.IntProp}");
+                });
+
+            processor.Parse(args);
+        }
+    }
+}
---
+++ src/CommandLineParserTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using System;
+
+namespace CommandLineParserTest
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('v', "verbose", Required = true, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
+        public class Options2
+        {
+            [Option('v', "verbose", Required = true, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default
+                .ParseArguments<Options, Options2>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Verbose)
+                    {
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+        }
+    }
+}
